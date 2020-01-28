2b5ef7a2c599c7c98de3fee06e74b68f45f436e2
sophie <sophiehuangx@gmail.com>
2020-01-28 13:35:13 -0500

    Set up 2 RecyclerViews for table Asks and table Bids Used Retrofit 2 to retrieve data from Bitstamp API Corrected the order of transactions used to plot the line chart in TransactionsFragment Set up a simple UI for PriceAlertFragment and started a class extending JobService from FirebaseJobDispatcher

--- app/src/main/java/sophiehuang/ilovezappos/OrderBookFragment.java
+++ app/src/main/java/sophiehuang/ilovezappos/OrderBookFragment.java
@@ -1,18 +1,124 @@
 package sophiehuang.ilovezappos;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import sophiehuang.ilovezappos.Model.ApiClient;
+import sophiehuang.ilovezappos.Model.BitstampJsonApi;
+import sophiehuang.ilovezappos.Model.OrderAdapter;
+import sophiehuang.ilovezappos.Model.OrderList;
 
 public class OrderBookFragment extends Fragment {
+
+    //declare variables
+    BitstampJsonApi bitstampJsonApi;
+    private OrderAdapter orderAdapter;
+    private RecyclerView recyclerView, recyclerView2;
+
+
+    List bids;
+    List asks;
+
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_order_book,container,false);
+        View view = inflater.inflate(R.layout.fragment_order_book, container, false);
+
+        bitstampJsonApi = ApiClient.getClient().create(BitstampJsonApi.class);
+        recyclerView = view.findViewById(R.id.recyclerView);
+        recyclerView2 = view.findViewById(R.id.recyclerView2);
+
+        setBidsData();
+        setAsksData();
+
+        return view;
+    }
+
+    private void setBidsData() {
+        Call<OrderList> ordersCall = bitstampJsonApi.getOrders();
+
+        ordersCall.enqueue(new Callback<OrderList>() {
+            @Override
+            public void onResponse(Call<OrderList> call, Response<OrderList> response) {
+                if (!response.isSuccessful()) {
+                    Toast.makeText(getActivity(), "Response not successful", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+
+                bids = response.body().getBids();
+                Toast.makeText(getActivity(), bids.size() + "", Toast.LENGTH_SHORT).show();
+
+                generateOrderInRecyclerView(bids, "bids");
+
+            }
+
+            @Override
+            public void onFailure(Call<OrderList> call, Throwable t) {
+                Toast.makeText(getActivity(), t.getMessage(), Toast.LENGTH_LONG).show();
+
+            }
+        });
+    }
+
+    private void setAsksData() {
+        Call<OrderList> ordersCall = bitstampJsonApi.getOrders();
+
+        ordersCall.enqueue(new Callback<OrderList>() {
+            @Override
+            public void onResponse(Call<OrderList> call, Response<OrderList> response) {
+                if (!response.isSuccessful()) {
+                    Toast.makeText(getActivity(), "Response not successful", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                Toast.makeText(getActivity(), "Response order book", Toast.LENGTH_SHORT).show();
+
+                asks = response.body().getAsks();
+
+                generateOrderInRecyclerView(asks, "asks");
+
+            }
+
+            @Override
+            public void onFailure(Call<OrderList> call, Throwable t) {
+                Toast.makeText(getActivity(), "Response order book", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+    }
+
+    private void generateOrderInRecyclerView(List orders, String orderType) {
+
+        orderAdapter = new OrderAdapter(orders);
+
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getActivity());
+
+        if (orderType.equals("bids")) {
+            recyclerView.setLayoutManager(layoutManager);
+            recyclerView.setAdapter(orderAdapter);
+        }
+        if (orderType.equals("asks")) {
+            recyclerView2.setLayoutManager(layoutManager);
+            recyclerView2.setAdapter(orderAdapter);
+        }
+
     }
 }
--- app/src/main/java/sophiehuang/ilovezappos/TransactionsFragment.java
+++ app/src/main/java/sophiehuang/ilovezappos/TransactionsFragment.java
@@ -4,15 +4,105 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+import sophiehuang.ilovezappos.Model.ApiClient;
+import sophiehuang.ilovezappos.Model.BitstampJsonApi;
+import sophiehuang.ilovezappos.Model.Transaction;
+
 public class TransactionsFragment extends Fragment {
+
+    LineChart linechart;
+    List<Entry> lineEntries;
+    BitstampJsonApi bitstampJsonApi;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_transactions,container,false);
+        View view = inflater.inflate(R.layout.fragment_transactions, container, false);
+
+        linechart = view.findViewById(R.id.lineChart);
+        lineEntries = new ArrayList<>();
+
+        linechart.setNoDataText("");
+        bitstampJsonApi = ApiClient.getClient().create(BitstampJsonApi.class);
+
+
+        setLineEntries();
+
+
+        return view;
+    }
+
+
+    //a function to get the bitstamp api from base url
+    private void setLineEntries() {
+        Call<List<Transaction>> call = bitstampJsonApi.getTransactions();
+        Toast.makeText(getActivity(), "ENTERING ENQUEUE", Toast.LENGTH_SHORT).show();
+        call.enqueue(new Callback<List<Transaction>>() {
+            @Override
+            public void onResponse(Call<List<Transaction>> call, Response<List<Transaction>> response) {
+                if (!response.isSuccessful()) {
+                    Toast.makeText(getActivity(), "Response 404", Toast.LENGTH_LONG).show();
+                    return;
+                }
+
+                //response.body() returns the a list of Transaction objects
+                //fields of Transaction objects are automatically filled
+                List<Transaction> transactions = response.body();
+
+                int count = 0;
+                for (int i = transactions.size() - 1; i >= 0; i--) {
+                    Transaction ts = transactions.get(i);
+                    String price = ts.getPrice();
+                    Float price_f = Float.parseFloat(price);
+                    lineEntries.add(new Entry(count, price_f));
+                    count++;
+                }
+
+
+                LineDataSet dataSet = new LineDataSet(lineEntries, "Recent Transaction History");
+                dataSet.setAxisDependency(YAxis.AxisDependency.LEFT);
+                dataSet.setHighlightEnabled(false);
+                dataSet.setCircleRadius(1.0f);
+
+                LineData data = new LineData(dataSet);
+                XAxis xAxis = linechart.getXAxis();
+                xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+                linechart.setTouchEnabled(true);
+                linechart.setData(data);
+                linechart.notifyDataSetChanged();
+                linechart.invalidate();
+            }
+
+            @Override
+            public void onFailure(Call<List<Transaction>> call, Throwable t) {
+                Toast.makeText(getActivity(), t.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        });
+
     }
 }
+
+
+
+
+
